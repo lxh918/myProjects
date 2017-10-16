21efe11e4aac2382704aa2e608122f3827dc3427
lxh918 <867053834@qq.com>
2017-10-16 21:24:49 +0800

    我的项目

---
+++ 黑白校园/webact/js/news.js
@@ -0,0 +1,613 @@
+var img1 = document.getElementById('img1')
+var img2 = document.getElementById('img2')
+var img3 = document.getElementById('img3')
+var img4 = document.getElementById('img4')
+function newover1()
+{
+    img1.src = 'webact/images/news/nt1.png'
+}
+function newover2()
+{
+    img1.src = 'webact/images/news/nt2.jpg'
+}
+function newover3()
+{
+    img1.src = 'webact/images/news/nt3.jpg'
+}
+function newover4()
+{
+    img1.src = 'webact/images/news/nt4.png'
+}
+function newover5()
+{
+    img1.src = 'webact/images/news/nt5.jpg'
+}
+function newover6()
+{
+    img1.src = 'webact/images/news/nt6.png'
+}
+function newover7()
+{
+    img2.src = 'webact/images/news/nt7.jpg'
+}
+function newover8()
+{
+    img2.src = 'webact/images/news/nt8.jpg'
+}
+function newover9()
+{
+    img2.src = 'webact/images/news/nt9.png'
+}
+function newover10()
+{
+    img2.src = 'webact/images/news/nt10.jpg'
+}
+function newover11()
+{
+    img2.src = 'webact/images/news/nt11.jpg'
+}
+function newover12()
+{
+    img2.src = 'webact/images/news/nt12.png'
+}
+function newover13()
+{
+    img3.src = 'webact/images/news/nt13.png'
+}
+function newover14()
+{
+    img3.src = 'webact/images/news/nt14.jpg'
+}
+function newover15()
+{
+    img3.src = 'webact/images/news/nt15.jpg'
+}
+function newover16()
+{
+    img3.src = 'webact/images/news/nt16.png'
+}
+function newover17()
+{
+    img3.src = 'webact/images/news/nt17.png'
+}
+function newover18()
+{
+    img3.src = 'webact/images/news/nt18.png'
+}
+function newover19()
+{
+}
+
+var img5 = document.getElementById('img5')
+var img6 = document.getElementById('img6')
+var img7 = document.getElementById('img7')
+var img8 = document.getElementById('img8')
+function newover20()
+{
+    img5.src = 'webact/images/news/nb1.jpg'
+}
+function newover21()
+{
+    img5.src = 'webact/images/news/nb2.jpg'
+}
+function newover22()
+{
+    img5.src = 'webact/images/news/nb3.jpg'
+}
+function newover23()
+{
+    img5.src = 'webact/images/news/nb4.jpg'
+}
+function newover24()
+{
+    img5.src = 'webact/images/news/nb5.jpg'
+}
+function newover25()
+{
+    img5.src = 'webact/images/news/nb6.jpg'
+}
+function newover26()
+{
+    img6.src = 'webact/images/news/nb7.jpg'
+}
+function newover27()
+{
+    img6.src = 'webact/images/news/nb8.jpg'
+}
+function newover28()
+{
+    img6.src = 'webact/images/news/nb9.jpg'
+}
+function newover29()
+{
+    img6.src = 'webact/images/news/nb10.jpg'
+}
+function newover30()
+{
+    img6.src = 'webact/images/news/nb11.jpg'
+}
+function newover31()
+{
+    img6.src = 'webact/images/news/nb12.jpg'
+}
+function newover32()
+{
+    img7.src = 'webact/images/news/nb13.jpg'
+}
+function newover33()
+{
+    img7.src = 'webact/images/news/nb14.jpg'
+}
+function newover34()
+{
+    img7.src = 'webact/images/news/nb15.jpg'
+}
+function newover35()
+{
+    img7.src = 'webact/images/news/nb16.jpg'
+}
+function newover36()
+{
+    img7.src = 'webact/images/news/nb17.jpg'
+}
+function newover37()
+{
+    img7.src = 'webact/images/news/nb18.jpg'
+}
+function newover38()
+{
+    img8.src = 'webact/images/news/nb19.jpg'
+}
+function newover39()
+{
+    img8.src = 'webact/images/news/nb20.jpg'
+}
+
+var img9 = document.getElementById('img9')
+var img10 = document.getElementById('img10')
+var img11 = document.getElementById('img11')
+var img12 = document.getElementById('img12')
+function newover40()
+{
+    img9.src = 'webact/images/news/nb21.jpg'
+}
+function newover41()
+{
+    img9.src = 'webact/images/news/nb22.jpg'
+}
+function newover42()
+{
+    img9.src = 'webact/images/news/nb23.jpg'
+}
+function newover43()
+{
+    img9.src = 'webact/images/news/nb24.jpg'
+}
+function newover44()
+{
+    img9.src = 'webact/images/news/nb25.jpg'
+}
+function newover45()
+{
+    img9.src = 'webact/images/news/nb26.jpg'
+}
+function newover46()
+{
+    img10.src = 'webact/images/news/nb27.jpg'
+}
+function newover47()
+{
+    img10.src = 'webact/images/news/nb28.jpg'
+}
+function newover48()
+{
+    img10.src = 'webact/images/news/nb29.jpg'
+}
+function newover49()
+{
+    img10.src = 'webact/images/news/nb30.jpg'
+}
+function newover50()
+{
+    img10.src = 'webact/images/news/nb31.jpg'
+}
+function newover51()
+{
+    img10.src = 'webact/images/news/nb32.jpg'
+}
+function newover52()
+{
+    img11.src = 'webact/images/news/nb33.jpg'
+}
+function newover53()
+{
+    img11.src = 'webact/images/news/nb34.jpg'
+}
+function newover54()
+{
+    img11.src = 'webact/images/news/nb35.jpg'
+}
+function newover55()
+{
+    img11.src = 'webact/images/news/nb36.jpg'
+}
+function newover56()
+{
+    img11.src = 'webact/images/news/nb37.jpg'
+}
+function newover57()
+{
+    img11.src = 'webact/images/news/nb38.jpg'
+}
+function newover58()
+{
+    img12.src = 'webact/images/news/nb39.jpg'
+}
+function newover59()
+{
+    img12.src = 'webact/images/news/nb40.jpg'
+}
+function newover60()
+{
+    img12.src = 'webact/images/news/nb41.jpg'
+}
+var img13 = document.getElementById('img13')
+var img14 = document.getElementById('img14')
+var img15 = document.getElementById('img15')
+var img16 = document.getElementById('img16')
+function newover61()
+{
+    img13.style.display = 'none'
+}
+function newout61()
+{
+    img13.style.display = 'block'
+}
+function newover62()
+{
+    img13.src = 'webact/images/news/nb42.jpg'
+}
+function newover63()
+{
+    img13.src = 'webact/images/news/nb43.jpg'
+}
+function newover64()
+{
+    img13.src = 'webact/images/news/nb44.jpg'
+}
+function newover65()
+{
+    img13.src = 'webact/images/news/nb45.jpg'
+}
+function newover66()
+{
+    img13.src = 'webact/images/news/nb46.jpg'
+}
+function newover67()
+{
+    img14.src = 'webact/images/news/nb47.jpg'
+}
+function newover68()
+{
+    img14.src = 'webact/images/news/nb48.jpg'
+}
+function newover69()
+{
+    img14.src = 'webact/images/news/nb49.jpg'
+}
+function newover70()
+{
+    img14.src = 'webact/images/news/nb50.jpg'
+}
+function newover71()
+{
+    img14.src = 'webact/images/news/nb51.jpg'
+}
+function newover72()
+{
+    img14.src = 'webact/images/news/nb52.jpg'
+}
+function newover73()
+{
+    img15.src = 'webact/images/news/nb54.jpg'
+}
+function newover74()
+{
+    img15.src = 'webact/images/news/nb55.jpg'
+}
+function newover75()
+{
+    img15.src = 'webact/images/news/nb56.jpg'
+}
+function newover76()
+{
+    img15.src = 'webact/images/news/nb57.jpg'
+}
+function newover77()
+{
+    img15.src = 'webact/images/news/nb58.jpg'
+}
+function newover78()
+{
+    img15.src = 'webact/images/news/nb59.jpg'
+}
+function newover79()
+{
+    img16.src = 'webact/images/news/nb60.jpg'
+}
+function newover80()
+{
+    img16.src = 'webact/images/news/nb61.jpg'
+}
+function newover81()
+{
+    img16.src = 'webact/images/news/nb62.jpg'
+}
+
+var img17 = document.getElementById('img17')
+var img18 = document.getElementById('img18')
+var img19 = document.getElementById('img19')
+var img20 = document.getElementById('img20')
+
+function newover82()
+{
+    img17.src = 'webact/images/news/nb63.jpg'
+}
+function newover83()
+{
+    img17.style.display = 'none'
+}
+function newout83()
+{
+    img17.style.display = 'block'
+}
+function newover84()
+{
+    img17.src = 'webact/images/news/nb65.jpg'
+}
+function newover85()
+{
+    img17.src = 'webact/images/news/nb66.jpg'
+}
+function newover86()
+{
+    img17.src = 'webact/images/news/nb67.jpg'
+}
+function newover87()
+{
+    img17.src = 'webact/images/news/nb68.png'
+}
+function newover88()
+{
+    img18.style.display = 'none'
+}
+function newout88()
+{
+    img18.style.display = 'block'
+}
+function newover89()
+{
+    img18.src = 'webact/images/news/nb70.jpg'
+}
+function newover90()
+{
+    img18.style.display = 'none'
+}
+function newout90()
+{
+    img18.style.display = 'block'
+}
+function newover91()
+{
+    img18.src = 'webact/images/news/nb72.jpg'
+}
+function newover92()
+{
+    img18.src = 'webact/images/news/nb73.jpg'
+}
+function newover93()
+{
+    img18.src = 'webact/images/news/nb74.jpg'
+}
+function newover94()
+{
+    img19.src = 'webact/images/news/nb75.jpg'
+}
+function newover95()
+{
+    img19.src = 'webact/images/news/nb76.jpg'
+}
+function newover96()
+{
+    img19.src = 'webact/images/news/nb77.jpg'
+}
+function newover97()
+{
+    img19.src = 'webact/images/news/nb78.jpg'
+}
+function newover98()
+{
+    img19.src = 'webact/images/news/nb79.jpg'
+}
+function newover99()
+{
+    img19.src = 'webact/images/news/nb80.jpg'
+}
+function newover100()
+{
+    img20.src = 'webact/images/news/nb81.jpg'
+}
+function newover101()
+{
+    img20.src = 'webact/images/news/nb82.png'
+}
+
+var img21 = document.getElementById('img21')
+var img22 = document.getElementById('img22')
+var img23 = document.getElementById('img23')
+var img24 = document.getElementById('img24')
+function newover102()
+{
+    img21.src = 'webact/images/news/nb83.jpg'
+}
+function newover103()
+{
+    img21.src = 'webact/images/news/nb84.jpg'
+}
+function newover104()
+{
+    img21.src = 'webact/images/news/nb85.jpg'
+}
+function newover105()
+{
+    img21.src = 'webact/images/news/nb86.png'
+}
+function newover106()
+{
+    img21.src = 'webact/images/news/nb87.jpg'
+}
+function newover107()
+{
+    img21.src = 'webact/images/news/nb88.jpg'
+}
+function newover108()
+{
+    img22.src = 'webact/images/news/nb89.jpg'
+}
+function newover109()
+{
+    img22.src = 'webact/images/news/nb90.jpg'
+}
+function newover110()
+{
+    img22.src = 'webact/images/news/nb91.jpg'
+}
+function newover111()
+{
+    img22.src = 'webact/images/news/nb92.jpg'
+}
+function newover112()
+{
+    img22.src = 'webact/images/news/nb93.jpg'
+}
+function newover113()
+{
+    img22.src = 'webact/images/news/nb94.jpg'
+}
+function newover114()
+{
+    img23.src = 'webact/images/news/nb95.jpg'
+}
+function newover115()
+{
+    img23.src = 'webact/images/news/nb96.jpg'
+}
+function newover116()
+{
+    img23.src = 'webact/images/news/nb97.jpg'
+}
+function newover117()
+{
+    img23.src = 'webact/images/news/nb98.jpg'
+}
+function newover118()
+{
+    img23.src = 'webact/images/news/nb99.jpg'
+}
+function newover119()
+{
+    img23.src = 'webact/images/news/nb100.jpg'
+}
+function newover120()
+{
+    img24.src = 'webact/images/news/nb101.jpg'
+}
+function newover121()
+{
+    img24.src = 'webact/images/news/nb102.jpg'
+}
+
+var img25 = document.getElementById('img25')
+var img26 = document.getElementById('img26')
+var img27 = document.getElementById('img27')
+var img28 = document.getElementById('img28')
+function newover122()
+{
+    img25.src = 'webact/images/news/nb103.jpg'
+}
+function newover123()
+{
+    img25.src = 'webact/images/news/nb104.jpg'
+}
+function newover124()
+{
+    img25.src = 'webact/images/news/nb105.jpg'
+}
+function newover125()
+{
+    img25.src = 'webact/images/news/nb106.jpg'
+}
+function newover126()
+{
+    img25.src = 'webact/images/news/nb107.jpg'
+}
+function newover127()
+{
+    img25.src = 'webact/images/news/nb108.png'
+}
+function newover128()
+{
+    img26.src = 'webact/images/news/nb109.jpg'
+}
+function newover129()
+{
+    img26.src = 'webact/images/news/nb110.jpg'
+}
+function newover130()
+{
+    img26.src = 'webact/images/news/nb111.png'
+}
+function newover131()
+{
+    img26.src = 'webact/images/news/nb112.jpg'
+}
+function newover132()
+{
+    img26.src = 'webact/images/news/nb113.jpg'
+}
+function newover133()
+{
+    img26.src = 'webact/images/news/nb114.jpg'
+}
+function newover134()
+{
+    img27.src = 'webact/images/news/nb115.jpg'
+}
+function newover135()
+{
+    img27.src = 'webact/images/news/nb116.jpg'
+}
+function newover136()
+{
+    img27.src = 'webact/images/news/nb117.jpg'
+}
+function newover137()
+{
+    img27.src = 'webact/images/news/nb118.jpg'
+}
+function newover138()
+{
+    img27.src = 'webact/images/news/nb119.jpg'
+}
+function newover139()
+{
+    img27.src = 'webact/images/news/nb120.jpg'
+}
+function newover140()
+{
+    img28.src = 'webact/images/news/nb121.jpg'
+}
+function newover141()
+{
+    img28.src = 'webact/images/news/nb122.jpg'
+}
